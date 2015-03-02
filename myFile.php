dcb13564e55d11c302023025ef76c584f090ef85
Ahmed BOUHANNA <abouhanna@cdta.dz>
2015-03-02 10:20:49 +0100

    Issuses 1 Fixes #1

--- myFile.php
+++ myFile.php
@@ -2,9 +2,5 @@ Premiere ligne master
 Ajout 2 emme ligne
 je suis au niveau du master
 je suis dans la nouvelle branch dev
-<<<<<<< HEAD
-encore dans dev		
-=======
-encore dans dev
-nouvelle ligne dev
->>>>>>> dev
+
+Issuses 1 fixed
